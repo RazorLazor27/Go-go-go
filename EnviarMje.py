67962fff253478c15b243d712ad5a5960ff7cbf4
javiwasabi <93001196+javiwasabi@users.noreply.github.com>
2024-04-11 17:48:06 -0400

    Update EnviarMje.py

--- EnviarMje.py
+++ EnviarMje.py
@@ -10,36 +10,44 @@ clientSocket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
 
 # Solicito mensajes y luego ejecuto las funciones para conectar al servidor y realizar el intercambio de mensajes.
 
-toSend = input("Qué deseas hacer? \nMarca 1 si quieres agregar tu información, Marca 2 en caso de querer la dirección IP de un nombre de dominio, \nEn caso de que quieras salir escribe STOP: \n")
 
 
+toSend = input("Qué deseas hacer? \nMarca 1 si quieres agregar tu información\nMarca 2 en caso de querer la dirección IP de un nombre de dominio, \nEn caso de que quieras salir escribe STOP: \n")
+flag = True
 
-if toSend == "STOP":
-	clientSocket.sendto(toSend.encode(), (serverAddr,serverPort))
-	print("Programa Finalizado")
-if toSend == "1":
+while flag:
 
-	nombre_dominio = input("Ingrese el Nombre de dominio :\n>")
-	Ip_direction = input("Ingrese Dirección IP :\n>")
-	ttl = input("Ingrese TTL :\n>")
-	type = input("Ingrese el Tipo :\n>")
+	if toSend == "STOP":
+		clientSocket.sendto(toSend.encode(), (serverAddr,serverPort))
+		print("Programa Finalizado")
+		flag = True
+	if toSend == "1":
 
-	print()
+		nombre_dominio = input("Ingrese el Nombre de dominio :\n>")
+		Ip_direction = input("Ingrese Dirección IP :\n>")
+		ttl = input("Ingrese TTL :\n>")
+		type = input("Ingrese el Tipo :\n>")
 
-	clientSocket.sendto(nombre_dominio.encode(), (serverAddr,serverPort))
-	clientSocket.sendto(Ip_direction.encode(), (serverAddr,serverPort))
-	clientSocket.sendto(ttl.encode(), (serverAddr,serverPort))
-	clientSocket.sendto(type.encode(), (serverAddr,serverPort))
+		print()
 
-	msg, addr = clientSocket.recvfrom(1024)
-	print(msg.decode()) 
+		clientSocket.sendto(nombre_dominio.encode(), (serverAddr,serverPort))
+		clientSocket.sendto(Ip_direction.encode(), (serverAddr,serverPort))
+		clientSocket.sendto(ttl.encode(), (serverAddr,serverPort))
+		clientSocket.sendto(type.encode(), (serverAddr,serverPort))
 
-if toSend == "2":
-	print()
+		msg, addr = clientSocket.recvfrom(1024)
+		print(msg.decode()) 
 
-	nombre_dominio = input("Ingrese el Nombre de dominio :\n>")
+	if toSend == "2":
+		print()
 
-  #Falta poner el sistema de imprimir en pantalla la respuesta del servidor dado el nombre de dominio (debe devolver la direccion ip)
+		nombre_dominio = input("Ingrese el Nombre de dominio :\n>")
+		clientSocket.sendto(nombre_dominio.encode(), (serverAddr,serverPort))
 
-	msg, addr = clientSocket.recvfrom(1024)
-	print(msg.decode()) 
+	#Falta poner el sistema de imprimir en pantalla la respuesta del servidor dado el nombre de dominio (debe devolver la direccion ip)
+
+
+		msg, addr = clientSocket.recvfrom(1024)
+		print(msg.decode()) 
+	toSend = input("Qué deseas hacer? \nMarca 1 si quieres agregar tu información\nMarca 2 en caso de querer la dirección IP de un nombre de dominio, \nEn caso de que quieras salir escribe STOP: \n")
+
